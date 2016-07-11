77032098555f3ddf806d7a3509c1b1c250f0ff63
Mohit Kumar <Mohit Kumar>
2016-07-11 23:16:28 +0530

    initial commit dex

---
+++ Assets_dex/GEText_Manager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class GEText_Manager : MonoBehaviour {
+
+
+    public static string getreq;
+    Text text;
+    // Use this for initialization
+    void Start () {
+        string url = "http://192.168.137.31:9000/api/comments";
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www));
+        text = GetComponent<Text>();
+
+
+    }
+
+    private IEnumerator WaitForRequest(WWW www)
+    {
+        //throw new NotImplementedException();
+        yield return www;
+        if (www.error == null)
+        {
+            Debug.Log("WWW OK : " + www.text);
+            getreq = www.text;
+
+        }else
+        {
+            Debug.Log("WWW Error" + www.error);
+        }
+    }
+
+
+/*    void Awake()
+    {
+        text = GetComponent<Text>();
+        score = 0;
+    }
+    */
+
+    void Update()
+    {
+        Debug.Log(getreq.ToString());
+        text.text = "DATA: " + getreq.ToString()[14];
+    }
+}
